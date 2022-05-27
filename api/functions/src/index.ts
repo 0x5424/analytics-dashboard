a866f26f3e9932bc83297d1fbb0d93c320853905
Trevor <trevor@osrs-stat.com>
2022-05-27 23:30:06 +0900

    Use bodyParser & cors middleware

--- api/functions/src/index.ts
+++ api/functions/src/index.ts
@@ -1,9 +1,13 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 import * as express from 'express'
+import * as cors from 'cors'
+import * as bodyParser from 'body-parser'
 
 admin.initializeApp()
 const app = express()
+app.use(bodyParser.json())
+app.use(cors({ origin: true }))
 
 app.get('/', (req: express.Request, res: express.Response) => {
   return res.status(200).send('success')
